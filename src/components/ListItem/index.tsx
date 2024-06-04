1be95374d0c4bb307d198f8f7f7b66c8cd1b17d0
unknown <jefferson.crn@gmail.com>
2024-06-04 16:53:31 -0300

    feat: add ListItem component

--- src/components/ListItem/index.tsx
+++ src/components/ListItem/index.tsx
@@ -4,7 +4,14 @@ export default function ListItem() {
     return (
         <>
             <div className="container-result-list">
-                <h1>List Items</h1>
+                <div className="dsf-output-item">
+                    <output className="dsf-output-item-name">
+                        item list
+                    </output>
+                    <output className="dsf-output-item-value">
+                        R$ 1200.00
+                    </output>
+                </div>
             </div>
         </>
     );
