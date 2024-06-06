772eff85f83bc2b56c75502e29ff18c2855a7432
unknown <jefferson.crn@gmail.com>
2024-06-05 22:52:41 -0300

    feat: add list item

--- src/components/ListingBody/index.tsx
+++ src/components/ListingBody/index.tsx
@@ -1,14 +1,27 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { ProductDTO } from "../../models/product";
 import * as productService from '../../services/product-service';
 import Filter from "./Filter";
 import ListItem from "./ListItem";
 import './style.css';
 
+type FormPrice = {
+    min: number;
+    max: number;
+}
 
 export default function ListingBody() {
 
-    const [products] = useState<ProductDTO[]>(productService.listProduct);
+    const [products, setProducts] = useState<ProductDTO[]>(productService.listProduct);
+
+    const [formPrice, setFormPrice] = useState<FormPrice>({
+        min: 0,
+        max: 1999
+    });
+
+    useEffect(() => {
+        setProducts(productService.findByPrice(formPrice.min, formPrice.max));
+    }, [formPrice.min, formPrice.max]);
 
     return (
         <>
@@ -16,7 +29,8 @@ export default function ListingBody() {
             <div className="container-result-list">
                 {
                     products.map(
-                        product => <ListItem key={product.id} product={product} />
+                        product =>
+                            <ListItem key={product.id} product={product} />
                     )
                 }
             </div>
